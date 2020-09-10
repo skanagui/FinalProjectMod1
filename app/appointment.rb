149e123a3eb43b6e18764d6a15afc8d9b122f387
skanagui <s.kanagui@gmail.com>
2020-09-10 18:07:59 -0400

    classes, migrations, added tty prompt

--- app/appointment.rb
+++ app/appointment.rb
@@ -1,5 +1,5 @@
 class Appointment < ActiveRecord::Base 
-      belongs_to :user
-      belongs_to :barber
+    belongs_to :user
+    belongs_to :barber
 
 end 
--- app/user.rb
+++ app/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base 
-      has_many :appointments
+    has_many :appointments
 
 
     def user_name 
