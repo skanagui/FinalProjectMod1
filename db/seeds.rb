576588a016bed790868377fac07ae1df5421ff55
SDBoahen <SDBoahen@gmail.com>
2020-09-10 13:09:52 -0400

    Seeds Planted

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,34 @@
-barber = Barber.new("Name")
+#### Barbers
+
+jamal    = Barber.create("Jamal",    3)
+jeronimo = Barber.create("Jeronimo", 7)
+sidney   = Barber.create("Sidney",   3)
+wendy    = Barber.create("Wendy",    2)
+zora     = Barber.create("Zora",    16)
+
+####  (name, experience) 
+
+
+
+
+#### Barbershops
+
+shop1 = Barbershop.create("Shop A", 4)
+shop2 = Barbershop.create("Shop B", 3)
+shop3 = Barbershop.create("Shop C", 4)
+shop4 = Barbershop.create("Shop D", 5)
+
+####  (name, rating)
+
+
+
+
+#### Appointments
+
+appo1 = Appointment.create("Booked",     user,    jamal)
+appo2 = Appointment.create("Booked",     user, jeronimo)
+appo3 = Appointment.create("Booked",     user, jeronimo)
+appo4 = Appointment.create("Booked",     user,    wendy)
+appo5 = Appointment.create("Not Booked", user,     zora)
+
+####  (IS BOOKED?, user, barber)
